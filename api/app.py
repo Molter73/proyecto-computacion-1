cea0acfed8b62aebbf5fc9201183cc14dcd126c3
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2024-01-15 19:27:50 +0100

    Configuración para api detrás de un proxy

--- api/app.py
+++ api/app.py
@@ -1,6 +1,15 @@
+import os
+
 from flask import Flask, request, abort, jsonify
 
 app = Flask(__name__)
+
+# Configuración del proxy
+proxy = os.environ.get("PROXY", "false")
+if proxy == "true":
+    from werkzeug.middleware.proxy_fix import ProxyFix
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1,
+                            x_proto=1, x_host=1, x_prefix=1)
 
 
 @app.route("/", methods=["POST"])
