b943c769ba4d03dd033fe553c4a7656099e8d579
Brenda Solorzano <bssolben@hotmail.com>
2024-01-21 23:47:05 +0100

    Interfaz modificada para integrar con la API y mostrar los resultados correspondientes

--- interfaz/interfaz.py
+++ interfaz/interfaz.py
@@ -2,13 +2,15 @@ from dash import Dash, html, dcc, dash_table, Output, Input, callback
 import plotly.express as px
 import pandas as pd
 
+# from api import api
+
 df = pd.read_csv(
     "https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv"
 )
 # Inicializamos la aplicación
-aplicacion = Dash(__name__)
+aplication = Dash(__name__)
 
-aplicacion.layout = html.Div(
+aplication.layout = html.Div(
     [
         html.Link(
             href="https://fonts.googleapis.com/css2?family=PT+Serif:wght@700&family=Source+Serif+4:opsz@8..60&display=swap",
@@ -23,16 +25,22 @@ aplicacion.layout = html.Div(
             ]
         ),
         html.Div(
-            [html.H3(className="titulo_opciones", children="Seleccionar una tarea MGT")]
+            [html.H3(className="titulo_opciones", children="Selecciona un modelo")]
         ),
         html.Div(
             className="opciones",
             children=dcc.RadioItems(
                 options=[
-                    "Identificación de textos generados",
-                    "Atribución de textos a modelos de generación",
+                    {
+                        "label": "Identificación de textos generados",
+                        "value": "identification",
+                    },
+                    {
+                        "label": "Atribución de textos a modelos de generación",
+                        "value": "attribution",
+                    },
                 ],
-                id="control_opciones",
+                id="seleccion_modelo",
             ),
         ),
         html.Div([html.H3(className="titulo_opciones", children="Introduce tu texto")]),
@@ -47,7 +55,8 @@ aplicacion.layout = html.Div(
                 html.H3(
                     className="titulo_opciones",
                     children="Resultados de la clasificación MGT",
-                )
+                ),
+                html.Div(id="resultados_metricas"),
             ]
         ),
         html.Div(
@@ -82,17 +91,34 @@ aplicacion.layout = html.Div(
 
 # Se llamará a esta función cada vez que se cambie el valor de la entrada para actualizar la salida.
 @callback(
-    Output(component_id="datos", component_property="figure"),
-    Input(component_id="control_opciones", component_property="value"),
+    # Output(component_id="datos", component_property="figure"),
+    Output(component_id="resultados_metricas", component_property="children"),
+    Input(component_id="button_txt_area", component_property="value"),
 )
-def control_grafica(op_elegida):
+def graphic_features(op_elegida):
     grafica = px.histogram(
         df, x="continent", y=op_elegida, histfunc="avg", color="green"
     )
     return grafica
 
 
+def model_selection(text: str, sel_model):
+    if sel_model == "identification":
+        return identification(text)
+    elif sel_model == "attribution":
+        return attribution(text)
+
+
+# def push_button():
+
+
+"""def get_metrics(text: str):
+    results = make_response(text)
+    return ...
+"""
+
+
 if __name__ == "__main__":
-    aplicacion.run(
+    aplication.run(
         debug=True
     )  # La aplicación se reiniciará automáticamente cada vez que se haga un cambio.
