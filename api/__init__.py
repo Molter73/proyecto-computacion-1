466d291eafaa1aa79ac1542ed3ae949b103aeb3f
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2024-01-26 17:55:52 +0100

    Compatibilizada API con gunicorn

--- api/__init__.py
+++ api/__init__.py
@@ -5,15 +5,16 @@ MODELS = {
     "identification": {},
     "attribution": {},
 }
-identification_pickle = "models/modelsA.pkl"
-attribution_pickle = "models/modelsB.pkl"
+
+models_subdir = os.environ.get("API_MODELS", "models")
+
+identification_pickle = os.path.join(models_subdir, "modelsA.pkl")
+attribution_pickle = os.path.join(models_subdir, "modelsB.pkl")
 
 if os.path.isfile(identification_pickle):
-    print("identification models")
     with open(identification_pickle, "rb") as f:
         MODELS["identification"]["CPU"] = pickle.load(f)
 
 if os.path.isfile(attribution_pickle):
-    print("attribution models")
     with open(attribution_pickle, "rb") as f:
         MODELS["attribution"]["CPU"] = pickle.load(f)
