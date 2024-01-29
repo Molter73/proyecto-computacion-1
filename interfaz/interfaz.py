45268701ae45052d26989cb5bdc2d6804e29afaf
Brenda Solorzano <bssolben@hotmail.com>
2024-01-29 20:33:30 +0100

    Cambios css

--- interfaz/interfaz.py
+++ interfaz/interfaz.py
@@ -4,17 +4,13 @@ import plotly.express as px
 import pandas as pd
 import requests
 
-df = pd.read_csv(
-    "https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv"
-)
-font = "https://fonts.googleapis.com/css2?family=PT+Serif:wght@700&family=Source+Serif+4:opsz@8..60&display=swap"
 # Inicializamos la aplicación
 application = Dash(__name__)
 
 application.layout = html.Div(
     [
         html.Link(
-            href=font,
+            href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@400;700;@900&display=swap",
             rel="stylesheet",
         ),
         html.Div(
@@ -25,43 +21,67 @@ application.layout = html.Div(
                 )
             ]
         ),
+        html.Div(className="separador"),
         html.Div(
-            [html.H3(className="titulo_opciones",
-                     children="Selecciona un modelo")]
+            [html.H3(className="titulo_opciones", children="Selecciona un modelo")]
         ),
         html.Div(
-            className="opciones",
-            children=dcc.RadioItems(
-                options=[
-                    {
-                        "label": "Identificación",
-                        "value": "identification",
-                    },
-                    {
-                        "label": "Atribución",
-                        "value": "attribution",
-                    },
-                ],
-                id="seleccion_modelo",
-            ),
+            [
+                html.H5(
+                    children="Get the best results in text prediction.",
+                    className="txt_d1",
+                ),
+                html.Div(
+                    className="opciones",
+                    children=dcc.RadioItems(
+                        options=[
+                            {
+                                "label": "Identificación",
+                                "value": "identification",
+                            },
+                            {
+                                "label": "Atribución",
+                                "value": "attribution",
+                            },
+                        ],
+                        id="seleccion_modelo",
+                    ),
+                ),
+            ],
+            className="opt_1",
         ),
-        html.Div([html.H3(className="titulo_opciones",
-                 children="Introduce tu texto")]),
-        html.Div([dcc.Textarea(className="txt_area", id="txt_area", value="")]),
-        html.Button(
-            className="boton_txt_analizar",
-            id="button_txt_area",
-            children="Analizar texto",
+        html.Div(className="espacios"),
+        html.Div(
+            [
+                html.Div(
+                    [
+                        html.H3(
+                            className="titulo_opciones", children="Introduce tu texto"
+                        )
+                    ]
+                ),
+                html.Div([dcc.Textarea(className="txt_area", id="txt_area", value="")]),
+                html.Button(
+                    className="boton_txt_analizar",
+                    id="button_txt_area",
+                    children="Analizar texto",
+                ),
+            ],
+            className="opt_2",
+        ),
+        html.Div(
+            [
+                html.H3(
+                    className="titulo_opciones",
+                    children="MGT classification results",
+                ),
+                html.Div(id="titulo_res_metricas"),
+                html.Div(id="resultados_metricas"),
+            ],
+            className="opti",
         ),
     ]
 )
-
-
-def graphic_features(op_elegida):
-    grafica = px.histogram(
-        df, x="continent", y=op_elegida, histfunc="avg", color="green"
-    )
-    return grafica
 
 
 # Se llamará a esta función cada vez que se cambie el valor de la entrada para actualizar la salida.
@@ -75,10 +95,9 @@ def model_selection(n_clicks, classification, text):
     if classification is None or text is None:
         raise PreventUpdate
 
-    response = requests.post("http://127.0.0.1:5000", json={
-        "text": text,
-        "classification": classification
-    })
+    response = requests.post(
+        "http://127.0.0.1:5000", json={"text": text, "classification": classification}
+    )
 
     if not response.ok:
         return html.P(response.json()["error"], className="res")
@@ -88,7 +107,8 @@ def model_selection(n_clicks, classification, text):
             "Modelo": name,
             "Label": values["label"],
             "Probabilidad": values["proba"] if values["proba"] is not None else "NA",
-        } for name, values in response.json()["predictions"].items()
+        }
+        for name, values in response.json()["predictions"].items()
     ]
 
     return dash_table.DataTable(
